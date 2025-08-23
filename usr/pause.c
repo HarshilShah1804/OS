776b776e446e53beb5014240b3178e8789e2205b
HarshilShah1804 <hkshah1978@gmail.com>
2025-08-23 21:30:31 +0530

    fix: remove undesired print statement

--- usr/pause.c
+++ usr/pause.c
@@ -2,7 +2,7 @@
 #include "stat.h"
 #include "user.h"
 
-#define TICKS_PER_SECOND 10  // To be verified
+#define TICKS_PER_SECOND 10
 
 int main(int argc, char **argv)
 {
@@ -16,7 +16,7 @@ int main(int argc, char **argv)
     // Convert `pause_duration` in seconds to `ticks`
     long ticks = TICKS_PER_SECOND * (long)pause_duration;
     
-    printf(1, "%d seconds\n", ticks);
+    // printf(1, "%d seconds\n", ticks);
     
     // Call sys_sleep for `ticks` ticks
     sleep((int)ticks);
